ed64cff427f605732f22e352ae2637fe092ea2ed
mozu888 <94517488+mozu888@users.noreply.github.com>
2021-12-21 17:31:42 +0900

    planモデルのバリデーションの設定

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,9 @@
 class Plan < ApplicationRecord
+  with_options presence true do
+    validates :title
+    validates :text
+    validates :time
+    validates :date
+  end
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
 end
