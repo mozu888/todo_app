97b69df4b9b2effd7bfbaa5ca5ec2a75a202f5b3
mozu888 <94517488+mozu888@users.noreply.github.com>
2021-12-28 12:37:57 +0900

    コントローラーの設定

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,13 @@
 class PlansController < ApplicationController
   def index
   end
+  def new
+    plan = Plan.new
+  end
+  def create
+    plan = Plan.new(plan_params)
+  end
+  private
+  def plan_params
+  end
 end
