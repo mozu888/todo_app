71c3118386e92b5c446a387ba88f55a1be790b24
mozu888 <94517488+mozu888@users.noreply.github.com>
2022-01-11 21:05:00 +0900

    モデルの修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,5 +5,5 @@ class Plan < ApplicationRecord
     validates :time
     validates :date
   end
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
 end
