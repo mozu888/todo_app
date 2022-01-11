1c8c8db6fab256cf661c4c026b4bec7ff0ae6006
mozu888 <94517488+mozu888@users.noreply.github.com>
2022-01-11 20:49:24 +0900

    Revert "Revert "モデルの修正""
    This reverts commit fb0f59aa6db4ec20f49b40ee07750462c8687e01.

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,12 +2,18 @@ class PlansController < ApplicationController
   def index
   end
   def new
-    plan = Plan.new
+    @plan = Plan.new
   end
   def create
-    plan = Plan.new(plan_params)
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   private
   def plan_params
+    params.require(:plan).permit(:title, :text, :category_id, :time, :date)
   end
 end
